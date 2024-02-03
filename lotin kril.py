b3902af21ff5989550e072115a168b9137974352
Makhmudovam <makhmudovam2711@gmail.com>
2024-02-03 11:40:03 +0500

    yoritiw

--- lotin kril.py
+++ lotin kril.py
@@ -1,3 +1,5 @@
 print('salom dunyo')
 a='bu mavzu koriniwidan murakkab lekin uddalab bolmas emas'
 print(a)
+b='qayta qayta koraverib kozlar ham organib ketarkan. miyyadan oldin kozlar iwlayaoti'
+print(a+' '+b)
